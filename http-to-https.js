8ea6681f47be4cc095f1bd56b0837c1586b52970
Joshua Klingbeil <github@joshuaklingbeil.com>
2018-06-07 01:14:40 -0500

    Update http-to-https.js

--- http-to-https.js
+++ http-to-https.js
@@ -7,7 +7,7 @@ if (window.location.protocol != 'https:') {
 
   var newLocation;                
 
-  newLocation = location.href.replace('http://', 'https://');   // Update location with "https" and load our var
-  newLocation = sslUrl.replace(stagingHTTP, stagingHTTPS);      // If in staging we'll need to expressly swap out ports
-  window.location = newLocation;                                // 
+  newLocation = location.href.replace('http://', 'https://');     // Update location with "https" and load our var
+  newLocation = newLocation.replace(stagingHTTP, stagingHTTPS);   // If in staging we'll need to expressly swap out ports
+  window.location = newLocation;                                   
 }
